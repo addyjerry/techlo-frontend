44f63751b57b9f8dc29f12737cbd3034ed78dae1
addyjerry <addyjerry2704@gmail.com>
2026-03-14 05:39:51 +0000

    middleware error fixed

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,10 @@
-export { default } from 'next-auth/middleware';
+import { withAuth } from 'next-auth/middleware';
+
+export default withAuth({
+  pages: {
+    signIn: '/login', // adjust to your actual login page
+  },
+});
 
 export const config = {
   matcher: ['/admin/:path*'],
